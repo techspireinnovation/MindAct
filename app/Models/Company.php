97ce65b8ee4f3550a43ae96e424bd30b7a796e9a
esprajwol <esprajwol@gmail.com>
2025-04-09 17:28:45 +0545

    company controller ship

---
+++ app/Models/Company.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    protected $fillable = [
+        'name',
+        'licence_issue_date',
+        'working_date',
+        'reg_number',
+        'pan_number',
+        'full_address',
+        'email_address',
+        'website',
+        'fax',
+        'logo',
+        'province',
+        'district',
+        'palika_name',
+        'ward_number',
+        'contact_person',
+        'contact_person_position',
+        'agreement_holder_name',
+        'phone',
+        'position',
+        'license_number',
+        'activation_key',
+        'url_link',
+    ];
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum', 'super.admin'])->group(function () {
-    Route::get('/profile', [AuthController::class, 'profile']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::middleware(['auth:sanctum', 'super.admin'])->prefix('admin')->group(function () {
+    Route::get('profile', [AuthController::class, 'profile']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::apiResource('companies', CompanyController::class);
 });

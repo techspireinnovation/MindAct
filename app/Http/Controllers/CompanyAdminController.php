6ddd14560ad07e8ef55bd2df8a346ab36a669fcc
esprajwol <esprajwol@gmail.com>
2025-04-10 12:56:45 +0545

    company controller files

---
+++ app/Http/Controllers/CompanyAdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class CompanyAdminController extends Controller
+{
+    // User Login API
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Login successful.',
+            'token' => $token,
+            'user' => $user,
+        ]);
+    }
+}

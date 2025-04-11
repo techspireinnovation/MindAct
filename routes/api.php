9f76448577324e0949e357a47fccb08508a396fa
esprajwol <esprajwol@gmail.com>
2025-04-11 11:01:11 +0545

    api lint

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyAdminController;
 use App\Http\Controllers\CompanyController;
-use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\FileUploadController;
+use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductTypeController;
 
 
@@ -21,19 +21,17 @@ Route::middleware(['auth:sanctum', 'super.admin'])->prefix('admin')->group(funct
     Route::get('profile', [AuthController::class, 'profile']);
     Route::get('logout', [AuthController::class, 'logout']);
     Route::apiResource('companies', CompanyController::class);
-    
+
 });
-    Route::post('/upload', [FileUploadController::class, 'upload']);
-    Route::get('/download/{filename}', [FileUploadController::class, 'download']);
+Route::post('/upload', [FileUploadController::class, 'upload']);
+Route::get('/download/{filename}', [FileUploadController::class, 'download']);
 
 
 
 // company admin operations
 Route::middleware(['auth:sanctum', 'company.admin'])->prefix('company')->group(function () {
     Route::get('profile', [CompanyAdminController::class, 'profile']);
-
     Route::apiResource('product-categories', ProductCategoryController::class);
-
     Route::resource('product-types', ProductTypeController::class);
 
 });

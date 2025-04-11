fadf68fc4371d6a570b698d620b040be3cccda3f
esprajwol <esprajwol@gmail.com>
2025-04-11 12:12:10 +0545

    measure model migration

--- database/migrations/2025_04_11_055922_add_measure_units_table.php
+++ database/migrations/2025_04_11_055922_add_measure_units_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('symbol')->nullable();
-            $table->int('quantity')->nullable();
+            $table->integer('quantity')->nullable();
             $table->foreignID('company_id')->constrained();
             $table->boolean('is_active')->default(true);
             $table->timestamps();

8915e74ec1ac11ba170a9ee14c5be3e4200528c7
esprajwol <esprajwol@gmail.com>
2025-04-16 14:13:19 +0545

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\ProductFieldValue;
+use DB;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
@@ -18,7 +20,7 @@ class ProductController extends Controller
     public function update(Request $request, $id): JsonResponse
     {
         try {
-            $item = Product::findOrFail($id);
+
             $validated = $request->validate([
                 'name' => 'required|string|max:255',
                 'is_active' => 'boolean|required',
@@ -26,21 +28,49 @@ class ProductController extends Controller
                 'category_id' => 'integer|exists:product_categories,id',
                 'brand_id' => 'integer|exists:brands,id',
                 'measure_unit_id' => 'integer|exists:measure_units,id',
-                'purchase_rate' => 'number',
-                'purchase_rate_vat' => 'number',
-                'retail_sales_price' => 'number',
-                'retail_sales_price_vat' => 'number',
-                'retail_sales_price_profit_percent' => 'number',
-                'wholesales_price' => 'number',
-                'wholesales_price_vat' => 'number',
-                'wholesales_price_profit_percent' => 'number',
+                'purchase_rate' => 'numeric',
+                'purchase_rate_vat' => 'numeric',
+                'retail_sales_price' => 'numeric',
+                'retail_sales_price_vat' => 'numeric',
+                'retail_sales_price_profit_percent' => 'numeric',
+                'wholesales_price' => 'numeric',
+                'wholesales_price_vat' => 'numeric',
+                'wholesales_price_profit_percent' => 'numeric',
                 'is_vatable' => 'boolean',
                 'product_type_id' => 'integer|exists:product_types,id',
                 'location_id' => 'integer|exists:locations,id',
-
+                'field_values' => 'required|array',
+                'field_values.*.product_field_id' => 'integer|exists:product_fields,id',
+                'field_values.*.value' => 'required|string|max:255',
             ]);
-            $item->update($validated);
-            return response()->json($item);
+
+            DB::transaction(function () use ($validated, $id) {
+                $product = Product::findOrFail($id);
+                $product->update($validated);
+
+                $existingProductIds = $product->productFieldValues()->pluck('id')->toArray();
+                $incomingProductIds = collect($validated['field_values'] ?? [])->pluck('id')->filter()->toArray();
+
+                // 🧼 Delete key values not in request
+                $fieldsValuesToDelete = array_diff($existingProductIds, $incomingProductIds);
+                ProductFieldValue::forceDestroy($fieldsValuesToDelete);
+
+                foreach ($validated['field_values'] ?? [] as $data) {
+                    if (isset($data['id'])) {
+                        // 🛠 Update existing comment
+                        $comment = ProductFieldValue::find($data['id']);
+                        $comment->update([
+                            'product_field_id' => $data['product_field_id'],
+                            'value' => $data['value'],
+                        ]);
+                    } else {
+                        // ➕ Create new comment
+                        $product->productFieldValues()->create($data);
+                    }
+                }
+            });
+            return response()->json(['message' => 'Product Updated']);
+
         } catch (ModelNotFoundException $e) {
             return response()->json(['error' => 'Item not found'], 404);
         } catch (QueryException $e) {

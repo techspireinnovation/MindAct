326f41b44cb95488d6b3756ef95c44d9bb31cc4d
esprajwol <esprajwol@gmail.com>
2025-04-22 15:07:08 +0545

    notification system lint

--- app/Notifications/DatabaseNotification.php
+++ app/Notifications/DatabaseNotification.php
@@ -35,16 +35,4 @@ class DatabaseNotification extends Notification implements ShouldQueue
             ->action('Notification Action', url('/'))
             ->line('Thank you for using our application!');
     }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(object $notifiable): array
-    {
-        return [
-            //
-        ];
-    }
 }

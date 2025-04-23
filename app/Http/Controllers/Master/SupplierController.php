f1959285841f19b0cbf80a97a4f85b4300086818
esprajwol <esprajwol@gmail.com>
2025-04-23 13:16:14 +0545

    suppliers controller

---
+++ app/Http/Controllers/Master/SupplierController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Supplier;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json(Brand::paginate(10));
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        try {
+            $item = Supplier::findOrFail($id);
+            $validated = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'string|max:255',
+                'pan_vat_number' => 'string|max:255',
+                'mobile' => 'required|string|max:255',
+                'address' => 'required|string|max:255',
+                'company_id' => 'integer|exists:companies,id'
+            ]);
+            $item->update($validated);
+            return response()->json($item);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Item not found'], 404);
+        } catch (QueryException $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'string|max:255',
+            'pan_vat_number' => 'string|max:255',
+            'mobile' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'company_id' => 'integer|exists:companies,id'
+        ]);
+
+        $item = Supplier::create($validated);
+        return response()->json($item, 201);
+    }
+
+    public function show($id): JsonResponse
+    {
+        try {
+            $item = Supplier::findOrFail($id);
+            return response()->json($item);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Item not found'], 404);
+        } catch (QueryException $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        try {
+            $item = Supplier::findOrFail($id);
+            $item->delete();
+            return response()->json(['message' => 'Supplier deleted']);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Item not found'], 404);
+        } catch (QueryException $e) {
+            return response()->json(['error' => 'An unexpected error occurred'], 500);
+        }
+    }
+}

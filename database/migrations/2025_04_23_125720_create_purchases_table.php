c2b6f2628a6d279d98c3c71872299146099ef7af
esprajwol <esprajwol@gmail.com>
2025-04-23 14:15:27 +0545

    purchase products

--- database/migrations/2025_04_23_125720_create_purchases_table.php
+++ database/migrations/2025_04_23_125720_create_purchases_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,20 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
+            $table->foreignID('company_id')->constrained('companies');
+            $table->string('mrn_number');
+            $table->string('bill_number')->nullable();
+            $table->string('pan_vat_number')->nullable();
+            $table->date('mrn_date');
+            $table->date('bill_date');
+            $table->foreignID('supplier_id')->constrained('suppliers');
+            $table->foreignID('location_id')->constrained('locations');
+            $table->double('discount_percent')->nullable();
+            $table->double('discount_percent_vat')->nullable();
+            $table->double('discount_amount_vat')->nullable();
+            $table->double('discount_amount')->nullable();
+            $table->double('roundoff_amount')->nullable();
+            $table->enum('payment_type', ['cash', 'bank', 'credit']);
             $table->timestamps();
         });
     }

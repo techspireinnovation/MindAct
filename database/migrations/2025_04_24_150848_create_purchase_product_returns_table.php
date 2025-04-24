987819ed50208781ee2e58ee876ba6cb43d1bbba
esprajwol <esprajwol@gmail.com>
2025-04-24 15:29:22 +0545

    get purchase product returns

---
+++ database/migrations/2025_04_24_150848_create_purchase_product_returns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_product_returns', function (Blueprint $table) {
+            $table->id();
+            $table->foreignID('company_id')->constrained('companies');
+            $table->foreignID(column: 'purchase_id')->constrained('purchases');
+            $table->foreignID(column: 'product_id')->constrained('products');
+            $table->double('quantity')->nullable();
+            $table->double('free_quantity')->nullable();
+            $table->double('price')->nullable();
+            $table->double('discount_percent')->nullable();
+            $table->double('discount_amount')->nullable();
+            $table->boolean('is_vatable')->nullable();
+            $table->foreignID(column: 'measure_unit_id')->constrained('measure_units');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_product_returns');
+    }
+};

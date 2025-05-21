6acfb146b1eb1702d069d4d7925ce987042fb60e
esprajwol <esprajwol@gmail.com>
2025-05-21 11:17:11 +0545

    journal voucher transaciton updated

--- app/Http/Controllers/JournalVoucherController.php
+++ app/Http/Controllers/JournalVoucherController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\JournalVoucher;
+use App\Models\JournalVoucherTransaction;
+use DB;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use Log;
 
 class JournalVoucherController extends Controller
 {
@@ -20,6 +23,80 @@ class JournalVoucherController extends Controller
         }
 
         return response()->json($query->paginate(50));
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        try {
+            $validated = $request->validate([
+                'voucher_number' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('journal_vouchers')->where(function ($query) use ($request, $id) {
+                        return $query->where('company_id', $request->company_id)->where('id', '!=', $id)->
+                            whereNull('deleted_at');
+
+                    }),
+                ],
+                'reference_number' => [
+                    'required',
+                    'string',
+                    'max:255',
+                    Rule::unique('journal_vouchers')->where(function ($query) use ($request, $id) {
+                        return $query->where('company_id', $request->company_id)->where('id', '!=', $id)
+                            ->whereNull('deleted_at');
+
+                    }),
+                ],
+                'project_id' => 'integer|exists:projects,id',
+                'salesman_id' => 'integer|exists:salesmen,id',
+                'transactions' => 'nullable|array',
+                'transactions.*.main_group_id' => 'nullable|integer|exists:measure_units,id',
+                'transactions.*.account_group_id' => 'nullable|integer|exists:measure_units,id',
+                'transactions.*.account_head_id' => 'nullable|integer|exists:measure_units,id',
+                'transactions.*.sub_group_id' => 'nullable|integer|exists:measure_units,id',
+                'transactions.*.account_code' => 'nullable|integer|exists:measure_units,id',
+                'transactions.*.particulars' => 'nullable|string|max:255',
+                'transactions.*.type' => 'nullable|string|max:255',
+                'transactions.*.debit' => 'nullable|numeric',
+                'transactions.*.credit' => 'nullable|numeric',
+                'company_id' => 'integer|exists:companies,id'
+            ]);
+
+            DB::transaction(function () use ($validated, $id, &$product) {
+                $product = JournalVoucher::findOrFail($id);
+                $product->update($validated);
+
+
+                // Handle field values
+                $existingFieldValueIds = $product->transactions()->pluck('id')->toArray();
+                $incomingFieldValueIds = collect($validated['transactions'] ?? [])->pluck('id')->filter()->toArray();
+
+                foreach ($validated['transactions'] as $childData) {
+                    if (isset($childData['id'])) {
+                        // Update existing child
+                        $child = JournalVoucherTransaction::find($childData['id']);
+                        $child->update($childData);
+                    } else {
+                        // Create new child
+                        $product->transactions()->create($childData);
+                    }
+                }
+                $fieldsValuesToDelete = array_diff($existingFieldValueIds, $incomingFieldValueIds);
+                JournalVoucherTransaction::whereIn('id', $fieldsValuesToDelete)->delete();
+
+            });
+
+            return response()->json(['message' => 'Journal Voucher Updated']);
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Item not found'], 404);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['error' => 'Update failed: ' . $e->getMessage()], 500);
+        }
+
     }
 
     public function store(Request $request): JsonResponse

4b54ee1e4bca5e44568635e73d7ebe9c7cd83073
esprajwol <esprajwol@gmail.com>
2025-05-23 16:44:38 +0545

    controller index

--- app/Http/Controllers/FixedAssetAccountController.php
+++ app/Http/Controllers/FixedAssetAccountController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FixedAssetAccount;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class FixedAssetAccountController extends Controller
 {
-    //
+    public function index(Request $request): JsonResponse
+    {
+        $query = FixedAssetAccount::query();
+
+        if ($request->has('keywords')) {
+            $query->where('name', 'LIKE', '%' . $request->input('keywords') . '%');
+        }
+
+        return response()->json($query->paginate(50));
+    }
 }

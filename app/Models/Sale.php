949b1a076d65c3e5288f04478304a4ac4461e702
karan_b_c <you@example.com>
2025-05-29 14:41:27 +0545

    sales return controller

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -31,6 +31,7 @@ class Sale extends Model
         'company_id',
         'customer_id',
         'customer_name',
+        'sale_id',
         'customer_address',
         'credit_days',
         'balance',

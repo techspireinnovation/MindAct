b38d919ebe2fe8ccf1822b131bf1571cfcb8d34e
esprajwol <esprajwol@gmail.com>
2025-06-05 15:33:18 +0545

    customer name

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -208,14 +208,14 @@ class ReportController extends Controller
             }
         })->get();
 
-        $purchaseItems = PurchaseProduct::select("purchase_products.id AS id", "purchase_products.quantity AS purchase_quantity", DB::raw('0 AS sale_quantity'), "purchase_products.product_id AS product_id", "purchase_products.customer_id AS customer_id", "purchases.invoice_date AS date")->leftJoin("purchases", "purchases.id", "=", "purchase_products.purchase_id")->leftJoin("customers", "customers.id", "=", "purchases.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
+        $purchaseItems = PurchaseProduct::select("purchase_products.id AS id", "purchase_products.quantity AS purchase_quantity", "customers.party_name AS customer_name", DB::raw('0 AS sale_quantity'), "purchase_products.product_id AS product_id", "purchase_products.customer_id AS customer_id", "purchases.invoice_date AS date")->leftJoin("purchases", "purchases.id", "=", "purchase_products.purchase_id")->leftJoin("customers", "customers.id", "=", "purchases.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
             if ($request->has('from_date') && $request->has('to_date')) {
                 $where->whereDate('purchase_products.created_at', '>=', $request->from_date)->whereDate('purchase_products.created_at', '<=', $request->to_date);
             }
         })->get();
         $purchaseItems->each->append(['primary_unit_name', 'average_rate']);
 
-        $purchaseReturnItems = PurchaseProductReturn::select("purchase_product_returns.id AS id", "purchase_product_returns.quantity AS purchase_return_quantity", DB::raw('0 AS sale_quantity'), "purchase_product_returns.product_id AS product_id", "purchase_product_returns.customer_id AS customer_id", "purchase_returns.invoice_date AS date")->leftJoin("purchase_returns", "purchase_returns.id", "=", "purchase_product_returns.purchase_return_id")->leftJoin("customers", "customers.id", "=", "purchase_returns.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
+        $purchaseReturnItems = PurchaseProductReturn::select("purchase_product_returns.id AS id", "customers.party_name AS customer_name", "purchase_product_returns.quantity AS purchase_return_quantity", DB::raw('0 AS sale_quantity'), "purchase_product_returns.product_id AS product_id", "purchase_product_returns.customer_id AS customer_id", "purchase_returns.invoice_date AS date")->leftJoin("purchase_returns", "purchase_returns.id", "=", "purchase_product_returns.purchase_return_id")->leftJoin("customers", "customers.id", "=", "purchase_returns.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
             if ($request->has('from_date') && $request->has('to_date')) {
                 $where->whereDate('purchase_product_returns.created_at', '>=', $request->from_date)->whereDate('purchase_product_returns.created_at', '<=', $request->to_date);
             }
@@ -223,7 +223,7 @@ class ReportController extends Controller
         $purchaseReturnItems->each->append(['primary_unit_name', 'average_rate']);
 
 
-        $saleItems = SaleProduct::select("sale_products.id AS id", "sale_products.quantity AS sale_quantity", DB::raw('0 AS purchase_quantity'), "sale_products.product_id AS product_id", "sales.customer_id AS customer_id", "sales.invoice_date AS date")->leftJoin("sales", "sales.id", "=", "sale_products.sale_id")->leftJoin("customers", "customers.id", "=", "sales.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
+        $saleItems = SaleProduct::select("sale_products.id AS id", "sale_products.quantity AS sale_quantity", "customers.party_name AS customer_name", DB::raw('0 AS purchase_quantity'), "sale_products.product_id AS product_id", "sales.customer_id AS customer_id", "sales.invoice_date AS date")->leftJoin("sales", "sales.id", "=", "sale_products.sale_id")->leftJoin("customers", "customers.id", "=", "sales.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
             if ($request->has('from_date') && $request->has('to_date')) {
                 $where->whereDate('sale_products.created_at', '>=', $request->from_date)->whereDate('sale_products.created_at', '<=', $request->to_date);
             }
@@ -231,7 +231,7 @@ class ReportController extends Controller
         $saleItems->each->append(['primary_unit_name', 'average_rate']);
 
 
-        $saleReturnItems = SalesReturnProduct::select("sales_return_products.id AS id", "sales_return_products.quantity AS sale_return_quantity", DB::raw('0 AS sale_quantity'), "sales_return_products.product_id AS product_id", "sales_returns.customer_id AS customer_id", "sales_returns.invoice_date AS date")->leftJoin("sales_returns", "sales_returns.id", "=", "sales_return_products.sales_return_id")->leftJoin("customers", "customers.id", "=", "sales_returns.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
+        $saleReturnItems = SalesReturnProduct::select("sales_return_products.id AS id", "sales_return_products.quantity AS sale_return_quantity", "customers.party_name AS customer_name", DB::raw('0 AS sale_quantity'), "sales_return_products.product_id AS product_id", "sales_returns.customer_id AS customer_id", "sales_returns.invoice_date AS date")->leftJoin("sales_returns", "sales_returns.id", "=", "sales_return_products.sales_return_id")->leftJoin("customers", "customers.id", "=", "sales_returns.customer_id")->where('product_id', $request->product_id)->where(function ($where) use ($request) {
             if ($request->has('from_date') && $request->has('to_date')) {
                 $where->whereDate('sales_return_products.created_at', '>=', $request->from_date)->whereDate('sales_return_products.created_at', '<=', $request->to_date);
             }

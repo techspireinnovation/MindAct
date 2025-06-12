8e91bfbd98847ae3451087fc998d9c532fa4f467
esprajwol <esprajwol@gmail.com>
2025-06-12 15:14:41 +0545

    sales vat amount

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -286,7 +286,7 @@ class ReportController extends Controller
 
         if ($request->type === "purchases") {
 
-            $items = Purchase::select("purchases.id", "purchases.invoice_date AS date", "purchases.total_amount as total_amount", "purchases.taxable_amount as taxable_amount", "purchases.purchase_bill_number as bill_number", "purchases.non_taxable_amount as non_taxable_amount", "purchases.customer_id", DB::raw('ROUND(purchases.vat_percent * .13,2) as vat_amount'))->with(relations: 'customer:id,party_name,pan_number')->orderBy('id', 'asc');
+            $items = Purchase::select("purchases.id", "purchases.invoice_date AS date", "purchases.total_amount as total_amount", "purchases.taxable_amount as taxable_amount", "purchases.purchase_bill_number as bill_number", "purchases.non_taxable_amount as non_taxable_amount", "purchases.customer_id", DB::raw('ROUND(purchases.vat_percent) as vat_amount'))->with(relations: 'customer:id,party_name,pan_number')->orderBy('id', 'asc');
 
             if ($request->has('month')) {
                 $items->whereMonth('invoice_date', $request->input('month'));
@@ -294,7 +294,7 @@ class ReportController extends Controller
             $items = $items->get();
 
         } else if ($request->type === "sales") {
-            $items = Sale::select("sales.id", "sales.invoice_date AS date", "sales.total_amount as total_amount", "sales.taxable_amount as taxable_amount", "sales.invoice_number as bill_number", "sales.non_taxable_amount as non_taxable_amount", "sales.customer_id")->with(relations: 'customer:id,party_name,pan_number')->orderBy('id', 'asc');
+            $items = Sale::select("sales.id", "sales.invoice_date AS date", "sales.total_amount as total_amount", "sales.taxable_amount as taxable_amount", "sales.invoice_number as bill_number", "sales.non_taxable_amount as non_taxable_amount", "sales.customer_id", DB::raw('ROUND(sales.taxable_amount * .13,2) as vat_amount'))->with(relations: 'customer:id,party_name,pan_number')->orderBy('id', 'asc');
 
             if ($request->has('month')) {
                 $items->whereMonth('invoice_date', $request->input('month'));

2e4b6e911e0f0e049de1250e0dfa6cd85422a5e4
esprajwol <esprajwol@gmail.com>
2025-06-20 11:38:11 +0545

    stock register list

--- app/Jobs/StockRegisterListExportJob.php
+++ app/Jobs/StockRegisterListExportJob.php
@@ -4,7 +4,9 @@ namespace App\Jobs;
 
 use App\Events\ReportEvent;
 use App\Helpers\Helper;
+use App\Models\Notification;
 use App\Reports\ProductReport;
+use Cache;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Rap2hpoutre\FastExcel\FastExcel;
@@ -14,15 +16,18 @@ use Str;
 class StockRegisterListExportJob implements ShouldQueue
 {
     use Queueable;
-    protected $request;
+
+    protected $requestUrl;
+    protected $tokenId;
     public $timeout = 300; // 5 minutes
 
     /**
      * Create a new job instance.
      */
-    public function __construct(array $request)
+    public function __construct(string $tokenId, string $requestUrl)
     {
-        $this->request = $request;
+        $this->tokenId = $tokenId;
+        $this->requestUrl = $requestUrl;
     }
 
     /**
@@ -31,35 +36,67 @@ class StockRegisterListExportJob implements ShouldQueue
     public function handle(): void
     {
         try {
+            $cacheKey = Helper::buildCacheKey($this->requestUrl);
+
+            $query = parse_url($this->requestUrl, PHP_URL_QUERY);
+            parse_str($query, $params);
+            $companyId = (int) $params['company_id'];
             $randomString = Str::random(5);
-            $filename = "stock_register_list_{$this->request['company_id']}_{$randomString}_" . now()->timestamp . ".xlsx";
 
-            $items = ProductReport::stockRegisterListDetails($this->request);
+            $filename = "stock_register_list_{$companyId}_{$randomString}_" . now()->timestamp . ".xlsx";
 
-            $sn = 1;
-            $rows = $items->cursor()->map(function ($item) use (&$sn) {
-                $last_purchase_rate_amount = Helper::getPrimaryRateAmount($item->id, $item->lastPurchase->id ?? 0);
-                return [
-                    'SN' => $sn++,
-                    'Product Id' => $item->product_unique_id,
-                    'Product Name' => $item->name,
-                    'HS Code' => optional($item->primaryProductItem)->hs_code,
-                    'Bar Code' => optional($item->primaryProductItem)->barcode,
-                    'UOM' => optional($item->primaryProductItem->measureUnit)->name,
-                    'Quantity' => $item->product_stock_quantity,
-                    'Rate With Vat' => round(Helper::getProductVatableAmount($item->id, $last_purchase_rate_amount ?? 0), 2),
-                    'Rate Without Vat' => round($last_purchase_rate_amount, 2),
-                    'Location' => optional($item->location)->name,
-                    'Category' => optional($item->category)->name,
-                    'Sub Category' => optional($item->subCategory)->name,
-                    'Brand' => optional($item->brand)->name,
-                    'Vat Type' => ($item->is_vatable) ? "Yes" : "No",
-                    'Product Type' => optional($item->productType)->name,
-                ];
-            })->collect();
+            if (Cache::has($cacheKey)) {
+                $compressed = Cache::get($cacheKey);
+                $rows = unserialize(gzuncompress($compressed));
+            } else {
+
+                $items = ProductReport::stockRegisterListDetails($params);
+
+                $sn = 1;
+                $rows = $items->cursor()->map(function ($item) use (&$sn) {
+                    $last_purchase_rate_amount = Helper::getPrimaryRateAmount($item->id, $item->lastPurchase->id ?? 0);
+                    return [
+                        'SN' => $sn++,
+                        'Product Id' => $item->product_unique_id,
+                        'Product Name' => $item->name,
+                        'HS Code' => optional($item->primaryProductItem)->hs_code,
+                        'Bar Code' => optional($item->primaryProductItem)->barcode,
+                        'UOM' => optional($item->primaryProductItem->measureUnit)->name,
+                        'Quantity' => $item->product_stock_quantity,
+                        'Rate With Vat' => round(Helper::getProductVatableAmount($item->id, $last_purchase_rate_amount ?? 0), 2),
+                        'Rate Without Vat' => round($last_purchase_rate_amount, 2),
+                        'Location' => optional($item->location)->name,
+                        'Category' => optional($item->category)->name,
+                        'Sub Category' => optional($item->subCategory)->name,
+                        'Brand' => optional($item->brand)->name,
+                        'Vat Type' => ($item->is_vatable) ? "Yes" : "No",
+                        'Product Type' => optional($item->productType)->name,
+                    ];
+                })->collect();
+
+                // compress and cached it
+                $compressed = gzcompress(serialize($rows));
+                Cache::remember($cacheKey, 3600, function () use ($compressed) {
+                    return $compressed;
+                });
+            }
 
             (new FastExcel($rows))->export(Storage::disk('company')->path($filename));
-            event(new ReportEvent($this->request['company_id'], ["stockRegisterListExportJob" => ['downloadCompleted' => true, 'fileUrl' => url("api/company/download-file/$filename")]]));
+
+            //create notifications to the user 
+            $notification = [
+                'user_id' => (int) $this->tokenId,
+                'type' => "DOWNLOAD",
+                "data" => [
+                    'title' => 'Stock Register List Export is completed.',
+                    'message' => 'Your stock register list has been successfully exported and is ready for download.',
+                    'url' => url("api/company/download-file/$filename"),
+                    'icon' => 'bell'
+                ]
+            ];
+            Notification::create($notification);
+
+            event(new ReportEvent($this->tokenId, ["stockRegisterListExportJob" => ['downloadCompleted' => true, 'fileUrl' => url("api/company/download-file/$filename")]]));
 
         } catch (\Exception $e) {
             \Log::error("---->> StockRegisterListExportJob Error <---");

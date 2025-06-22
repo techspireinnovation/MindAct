c8f65923e55391900b087a232419c703fb512f81
esprajwol <esprajwol@gmail.com>
2025-06-22 16:20:19 +0545

    tenant package

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\BroadcastServiceProvider::class,
     App\Providers\DropboxServiceProvider::class,
     Maatwebsite\Excel\ExcelServiceProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Tenant;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,6 +8,11 @@ Route::get('/', function () {
 });
 
 Route::get('/phpinfo', function () {
+    $tenant = Tenant::create([
+        'id' => 'company1234',
+        'data' => ['name' => 'Company 1234']
+    ]);
+
     ob_start();
     phpinfo();
     return response(ob_get_clean());

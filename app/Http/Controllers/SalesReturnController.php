b9d2d95d618f4e8727cf25608327bd078a5d52a1
karan_b_c <you@example.com>
2025-06-26 11:34:03 +0545

    update 1

--- app/Http/Controllers/SalesReturnController.php
+++ app/Http/Controllers/SalesReturnController.php
@@ -713,7 +713,7 @@ public function getSaleByRefNumber(Request $request): JsonResponse
                 'sql' => $e->getSql(),
                 'bindings' => $e->getBindings(),
             ]);
-            // dd($e->getMessage());
+           
             return response()->json(['error' => 'A database error occurred'], 500);
         } catch (\Exception $e) {
             Log::error('Unexpected error in getSaleByRefNumber', [

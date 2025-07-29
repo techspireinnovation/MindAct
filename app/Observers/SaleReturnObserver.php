d13db9a32eddeed43580c39c7df363c5a107261c
esprajwol <esprajwol@gmail.com>
2025-07-29 16:57:06 +0545

    sale ret ob

--- app/Observers/SaleReturnObserver.php
+++ app/Observers/SaleReturnObserver.php
@@ -20,13 +20,17 @@ class SaleReturnObserver
         $accGroup = AccountGroup::where('name', "Sales Return")->first();
         $accHead = AccountHead::where('name', "Sales Return")->first();
 
+        $customerName = isset($sale->customer) ? $saleReturn->customer->party_name : $saleReturn->customer_name;
+        $ledgerType = isset($sale->customer) ? $saleReturn->customer->ledger_type : 'custom';
+
+
         $voucher = VoucherSummary::firstOrCreate([
             'date' => $saleReturn->invoice_date,
             'date_bs' => $saleReturn->invoice_date_bs,
             'company_id' => $saleReturn->company_id,
             'branch_id' => null,
-            'voucher_number' => "SRVOU-828300{$saleReturn->invoice_number}",
-            'particulars' => "Product Sale Returned from {$saleReturn->customer->party_name} - Bill No. {$saleReturn->invoice_number}",
+            'voucher_number' => "SLVOU-828300{$saleReturn->invoice_number}",
+            'particulars' => "Product Sale Returned from {$customerName} - Bill No. {$saleReturn->invoice_number}",
             'debit' => $saleReturn->sub_total_before_discount,
             'credit' => 0,
             'tr_bill_number' => $saleReturn->id,
@@ -55,25 +59,25 @@ class SaleReturnObserver
             $purchaseAccGroups['Round Off Minus in Purchase'] = ['type' => 'credit', 'valueAmount' => $saleReturn->roundoff_amount, 'payment_type' => ''];
         }
 
-        switch ($saleReturn->customer->ledger_type) {
+        switch ($ledgerType) {
             case 'customer':
 
                 $partyAccountGroup = AccountGroup::where(['name' => "Accounts Receivable (Debtors)"])->orderBy('code', 'DESC')->first();
                 $code = $partyAccountGroup ? (int) $partyAccountGroup->code + 1 : 1;
-                $partyHead = AccountHead::where(['name' => $saleReturn->customer->party_name, 'company_id' => $saleReturn->company_id])->first();
+                $partyHead = AccountHead::where(['name' => $customerName, 'company_id' => $saleReturn->company_id])->first();
 
                 if (isset($sale->payment['credit']) && $saleReturn->payment['credit'] !== null)
-                    $saleAccGroups['Accounts Receivable (Debtors)'] = ['type' => 'credit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
+                    $purchaseAccGroups['Accounts Receivable (Debtors)'] = ['type' => 'credit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
                 break;
 
             case 'vendor':
             case 'both':
                 $partyAccountGroup = AccountGroup::where(['name' => "Accounts Payable (Creditors)"])->orderBy('code', 'DESC')->first();
                 $code = $partyAccountGroup ? (int) $partyAccountGroup->code + 1 : 1;
-                $partyHead = AccountHead::where(['name' => $saleReturn->customer->party_name, 'company_id' => $saleReturn->company_id])->first();
+                $partyHead = AccountHead::where(['name' => $customerName, 'company_id' => $saleReturn->company_id])->first();
 
                 if (isset($sale->payment['credit']) && $saleReturn->payment['credit'] !== null)
-                    $saleAccGroups['Accounts Payable (Creditors)'] = ['type' => 'credit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
+                    $purchaseAccGroups['Accounts Payable (Creditors)'] = ['type' => 'credit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
                 break;
 
             default:
@@ -82,7 +86,7 @@ class SaleReturnObserver
                 $partyHead = AccountHead::firstOrCreate(['name' => $saleReturn->customer_name, 'company_id' => $saleReturn->company_id, 'account_group_id' => $partyAccountGroup->id, 'is_active' => true, 'code' => $code, 'is_primary' => true]);
 
                 if (isset($saleReturn->payment['credit']) && $saleReturn->payment['credit'] !== null)
-                    $purchaseAccGroups['Accounts Payable (Creditors)'] = ['type' => 'debit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
+                    $purchaseAccGroups['Accounts Receivable (Debtors)'] = ['type' => 'debit', 'valueAmount' => (float) $saleReturn->payment["credit"], 'payment_type' => 'CREDIT'];
                 break;
         }
 
@@ -95,7 +99,7 @@ class SaleReturnObserver
                 if (!$accHead && ($purchaseAccGroupKey == "Accounts Receivable (Debtors)" || $purchaseAccGroupKey == "Accounts Payable (Creditors)")) {
                     $accountHead = AccountHead::where(['account_group_id' => $accGroup->id])->orderBy('code', 'DESC')->first();
                     $code = $accountHead ? (int) $accountHead->code + 1 : 1;
-                    $accHead = AccountHead::firstOrCreate(['name' => $saleReturn->customer->party_name, 'company_id' => $saleReturn->company_id, 'account_group_id' => $accGroup->id, 'is_active' => true, 'code' => $code, 'is_primary' => true]);
+                    $accHead = AccountHead::firstOrCreate(['name' => $customerName, 'company_id' => $saleReturn->company_id, 'account_group_id' => $accGroup->id, 'is_active' => true, 'code' => $code, 'is_primary' => true]);
 
                 }
 
@@ -106,8 +110,8 @@ class SaleReturnObserver
                         'date_bs' => $saleReturn->invoice_date_bs,
                         'company_id' => $saleReturn->company_id,
                         'branch_id' => null,
-                        'voucher_number' => "PRVOU-828300{$saleReturn->id}",
-                        'particulars' => "Product Sale Returned from {$saleReturn->customer->party_name} - Bill No. {$saleReturn->purchase_bill_number}",
+                        'voucher_number' => "SRVOU-828300{$saleReturn->id}",
+                        'particulars' => "Product Sale Returned from {$customerName} - Bill No. {$saleReturn->purchase_bill_number}",
                         'credit' => $purchaseAccGroupValue['type'] === 'debit' ? $purchaseAccGroupValue['valueAmount'] : 0,
                         'debit' => $purchaseAccGroupValue['type'] === 'credit' ? $purchaseAccGroupValue['valueAmount'] : 0,
                         'tr_bill_number' => $saleReturn->purchase_bill_number,
@@ -125,8 +129,8 @@ class SaleReturnObserver
                 'date_bs' => $saleReturn->invoice_date_bs,
                 'company_id' => $saleReturn->company_id,
                 'branch_id' => null,
-                'voucher_number' => "PRVOU-828300{$saleReturn->id}",
-                'particulars' => "Product Sale Returned from {$saleReturn->customer->party_name} - Bill No. {$saleReturn->purchase_bill_number}",
+                'voucher_number' => "SRVOU-828300{$saleReturn->id}",
+                'particulars' => "Product Sale Returned from {$customerName} - Bill No. {$saleReturn->purchase_bill_number}",
                 'debit' => 0,
                 'credit' => $saleReturn->total_amount,
                 'tr_bill_number' => $saleReturn->purchase_bill_number,
@@ -147,7 +151,7 @@ class SaleReturnObserver
 
 
             if (isset($saleReturn->payment['cash']) && $saleReturn->payment['cash'] !== null && $saleReturn->payment['cash'] > 0) {
-                $accHead = AccountHead::where(['name' => $saleReturn->customer->party_name, 'company_id' => $saleReturn->company_id])->first();
+                $accHead = AccountHead::where(['name' => $customerName, 'company_id' => $saleReturn->company_id])->first();
                 VoucherInnerDetail::create([
                     'voucher_summary_id' => $voucher->id,
                     'company_id' => $saleReturn->company_id,
@@ -159,7 +163,7 @@ class SaleReturnObserver
 
             if (isset($saleReturn->payment['bank']) && $saleReturn->payment['bank'] !== null && $saleReturn->payment['bank'] > 0) {
 
-                $accHead = AccountHead::where(['name' => $saleReturn->customer->party_name, 'company_id' => $saleReturn->company_id])->first();
+                $accHead = AccountHead::where(['name' => $customerName, 'company_id' => $saleReturn->company_id])->first();
                 VoucherInnerDetail::create([
                     'voucher_summary_id' => $voucher->id,
                     'company_id' => $saleReturn->company_id,

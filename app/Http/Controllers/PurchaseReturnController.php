2621be7746bd3b71bc18baa47f7a0ccafc4ab407
karan_b_c <you@example.com>
2025-07-30 15:37:48 +0545

    new backend update

--- app/Http/Controllers/PurchaseReturnController.php
+++ app/Http/Controllers/PurchaseReturnController.php
@@ -5445,7 +5445,7 @@ class PurchaseReturnController extends Controller
             return response()->json(['error' => 'An unexpected error occurred'], 500);
         } catch (\Exception $e) {
             \Log::error($e);
-            return response()->json(['error' => 'An unexpected error occurred'], 500);
+            return response()->json(['error' => 'An Unexpected error occurred'], 500);
         }
     }
 }

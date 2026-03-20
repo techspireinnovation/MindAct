dddbfdd85b63befb83d8f3071f452f07b2665f4d
Karan-kb <bckaran17@gmail.com>
2026-03-20 17:57:45 +0545

    stock migration added

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -94,7 +94,7 @@ class StockController extends Controller
             $data = $this->repository->delete($id);
             return response()->json(['message' => 'Stock deleted successfully', 'data' => $data], 201);
         } catch (ModelNotFoundException $e) {
-            return response()->json(['message' => 'Stock not found'], 404);
+            return response()->json(['message' => 'Stock not found !'], 404);
         } catch (\Exception $e) {
             return response()->json(['message' => 'An error occurred while creating the stock', 'error' => $e->getMessage()], 500);
         } catch (QueryException $e) {

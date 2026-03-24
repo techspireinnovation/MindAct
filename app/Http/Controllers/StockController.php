c589d6736e940035598a62bf90d2c5d3ce112737
Karan-kb <bckaran17@gmail.com>
2026-03-24 12:28:51 +0545

    added validation

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -44,7 +44,7 @@ class StockController extends Controller
         } catch (ModelNotFoundException $e) {
             return response()->json(['message' => 'Stock not found !!'], 404);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'An error occurred while creating the stock !', 'error' => $e->getMessage()], 500);
+            return response()->json(['message' => 'An error occurred while creating the stock !!', 'error' => $e->getMessage()], 500);
         } catch (QueryException $e) {
             return response()->json(['message' => 'Database error occurred while creating the stock !', 'error' => $e->getMessage()], 500);
         }

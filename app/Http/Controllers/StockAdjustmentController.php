f0e42bbe9f9552e54180ff02531d710354e32a70
Karan-kb <bckaran17@gmail.com>
2026-03-31 11:07:55 +0545

    updated migaretion

--- app/Http/Controllers/StockAdjustmentController.php
+++ app/Http/Controllers/StockAdjustmentController.php
@@ -27,7 +27,7 @@ class StockAdjustmentController extends Controller
         } catch (ModelNotFoundException $e) {
             return response()->json(['message' => 'Stock Adjsutment not found !!'], 404);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'An error occurred while creating the Stock Adjsutment', 'error' => $e->getMessage()], 500);
+            return response()->json(['message' => 'An error occurred while creating the Stock Adjsutment !!', 'error' => $e->getMessage()], 500);
         } catch (QueryException $e) {
             return response()->json(['message' => 'Database error occurred while creating the Stock Adjsutment!!', 'error' => $e->getMessage()], 500);
         }
